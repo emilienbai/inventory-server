92017488bf1192f2ce7b6581fb679a074cd4927a
emilienbai <bai.emilien+github@gmail.com>
2020-10-18 22:42:55 +0200

    Improve performance of build

--- src/web-client/src/app/app.module.ts
+++ src/web-client/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginController } from './components/login/LoginController';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [LoginController],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [LoginController]
 })
 export class AppModule {}
---
+++ src/web-client/src/app/components/login/LoginController.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: 'login.html',
+    styles: []
+})
+export class LoginController {
+    public title = 'web-client';
+}
